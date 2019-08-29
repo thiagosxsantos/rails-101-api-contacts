17601132cd6f2af111e0828f62c3cd584670d355
Thiago Sousa Santos <thiagos.xsantos@gmail.com>
2019-08-29 18:24:40 -0300

    FIX: move api_controller to correct folder

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  acts_as_token_authenticatable
+
   has_many :contacts, dependent: :destroy
 end
